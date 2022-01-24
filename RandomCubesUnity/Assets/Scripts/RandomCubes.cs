4419ddade6fb2c023647c04d882f1d3ddfe9baaa
Alex Jenkins <arj0288@utulsa.edu>
2022-01-24 16:35:13 -0600

    Random Cube Script
    -Created Cube Prefab
    -Started RandomCube script

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/****
+ * Created by: Alex Jenkins
+ * Date Created: Jan 24, 2022
+ * Last edited by: Alex Jenkins
+ * Last edited: Jan 24, 2022
+ * 
+ * Description: Spawns multiple cube prefabs into a scne
+ * ****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
